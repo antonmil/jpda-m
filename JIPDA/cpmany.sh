ed1b4dc88dad6838cbe14cd8a842e6201a929699
Anton Milan <devnull@localhost>
2015-05-01 09:07:05 +0930

    training

--- JIPDA/cpmany.sh
+++ JIPDA/cpmany.sh
@@ -1,23 +1,46 @@
 #!/bin/sh
 
-cp -R config/0422A-1/ config/0430Aa-1
-cp -R config/0422B-1/ config/0430Ba-1
-cp -R config/0422C-1/ config/0430Ca-1
-cp -R config/0422D-1/ config/0430Da-1
-cp -R config/0422E-1/ config/0430Ea-1
-cp -R config/0422G-1/ config/0430Fa-1
-cp -R config/0422G-1/ config/0430Ga-1
-cp -R config/0422H-1/ config/0430Ha-1
-cp -R config/0422I-1/ config/0430Ia-1
-cp -R config/0422J-1/ config/0430Ja-1
+cp -R ./config/0430Aa-1/ config/0430Ab-1
+cp -R ./config/0430Ba-1/ config/0430Bb-1
+cp -R ./config/0430Ca-1/ config/0430Cb-1
+cp -R ./config/0430Da-1/ config/0430Db-1
+cp -R ./config/0430Ea-1/ config/0430Eb-1
+cp -R ./config/0430Ga-1/ config/0430Fb-1
+cp -R ./config/0430Ga-1/ config/0430Gb-1
+cp -R ./config/0430Ha-1/ config/0430Hb-1
+cp -R ./config/0430Ia-1/ config/0430Ib-1
+cp -R ./config/0430Ja-1/ config/0430Jb-1
 
-cp config/0430-1/0001.ini config/0430Aa-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ba-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ca-1/0001.ini
-cp config/0430-1/0001.ini config/0430Da-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ea-1/0001.ini
-cp config/0430-1/0001.ini config/0430Fa-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ga-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ha-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ia-1/0001.ini
-cp config/0430-1/0001.ini config/0430Ja-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Ab-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Bb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Cb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Db-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Eb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Fb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Gb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Hb-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Ib-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Jb-1/0001.ini
+
+# cp -R ./config/0430Aa-1/ config/0430Ka-1
+# cp -R ./config/0430Ba-1/ config/0430La-1
+# cp -R ./config/0430Ca-1/ config/0430Ma-1
+# cp -R ./config/0430Da-1/ config/0430Na-1
+# cp -R ./config/0430Ea-1/ config/0430Oa-1
+# cp -R ./config/0430Ga-1/ config/0430Pa-1
+# cp -R ./config/0430Ga-1/ config/0430Qa-1
+# cp -R ./config/0430Ha-1/ config/0430Ra-1
+# cp -R ./config/0430Ia-1/ config/0430Sa-1
+# cp -R ./config/0430Ja-1/ config/0430Ta-1
+
+
+cp config/0430Da-3/0001.ini config/0430Ka-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430La-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Ma-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Na-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Oa-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Pa-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Qa-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Ra-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Sa-1/0001.ini
+cp config/0430Da-3/0001.ini config/0430Ta-1/0001.ini
--- JIPDA/submany.sh
+++ JIPDA/submany.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-./submitTrain.sh 0422Aa-1
-./submitTrain.sh 0422Ba-1
-./submitTrain.sh 0422Ca-1
-./submitTrain.sh 0422Da-1
-./submitTrain.sh 0422Ea-1
-./submitTrain.sh 0422Fa-1
-./submitTrain.sh 0422Ga-1
-./submitTrain.sh 0422Ha-1
-./submitTrain.sh 0422Ia-1
-./submitTrain.sh 0422Ja-1
+./submitTrain.sh 0430Ab-1
+./submitTrain.sh 0430Bb-1
+./submitTrain.sh 0430Cb-1
+./submitTrain.sh 0430Db-1
+./submitTrain.sh 0430Eb-1
+./submitTrain.sh 0430Fb-1
+./submitTrain.sh 0430Gb-1
+./submitTrain.sh 0430Hb-1
+./submitTrain.sh 0430Ib-1
+./submitTrain.sh 0430Jb-1
+
+# ./submitTrain.sh 0430Ka-1
+# ./submitTrain.sh 0430La-1
+# ./submitTrain.sh 0430Ma-1
+# ./submitTrain.sh 0430Na-1
+# ./submitTrain.sh 0430Oa-1
+# ./submitTrain.sh 0430Pa-1
+# ./submitTrain.sh 0430Qa-1
+# ./submitTrain.sh 0430Ra-1
+# ./submitTrain.sh 0430Sa-1
+# ./submitTrain.sh 0430Ta-1
